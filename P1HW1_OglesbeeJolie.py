1668f79df6c767ce80b403986a42f16e60c4b153
Solemn <oglesbej5579@student.faytechcc.edu>
2024-06-18 22:27:04 -0400

    P1HW1_OglesbeeJolie.py

---
+++ P1HW1_OglesbeeJolie.py
@@ -0,0 +1,13 @@
+# Exponentiation
+base = int(input("Enter an integer base: "))
+exponent = int(input("Enter an integer exponent: "))
+result = base ** exponent
+print(f"{base}^{exponent} = {result}!")
+
+# Integer Operations
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+num3 = int(input("Enter the third integer: "))
+sum_of_first_two = num1 + num2
+final_result = sum_of_first_two - num3
+print(f"Sum of {num1} and {num2} minus {num3} = {final_result}")
