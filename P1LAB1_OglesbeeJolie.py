ed32ced4f70a75289cec1982f62f28c845ca6a9e
Solemn <oglesbej5579@student.faytechcc.edu>
2024-06-18 22:11:41 -0400

    Add files via upload
    First Submission

---
+++ P1LAB1_OglesbeeJolie.py
@@ -0,0 +1,8 @@
+# Prompt the user for their first name
+first_name = input("Enter your first name: ")
+
+# Prompt the user for their last name
+last_name = input("Enter your last name: ")
+
+# Display a welcome message
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
